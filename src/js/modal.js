421635fd7efe685008f1cfca3fdf42c99ef299de
Marck-road <109259291+Marck-road@users.noreply.github.com>
2024-03-23 23:18:39 +0800

    Login and refactored tailwind.config

---
+++ src/js/modal.js
@@ -0,0 +1,24 @@
+
+function openModal(modalId, overlayId){
+    document.getElementById(modalId).style.display = 'flex';
+    document.getElementById(overlayId).style.display = 'flex';
+}
+
+function openSuccessModal(modalId, overlayId){
+    document.getElementById(modalId).style.display = 'flex';
+    document.getElementById(overlayId).style.display = 'flex';
+}
+
+function closeModal(modalId, overlayId) {
+    document.getElementById(modalId).style.display = 'none';
+    document.getElementById(overlayId).style.display = 'none';
+}
+
+function closeOpenModal(modalId, overlayId) {
+    document.getElementById(modalId).style.display = 'none';
+    document.getElementById(overlayId).style.display = 'none';
+    var openModal = modalId + 'apply';
+    var openOverlay = overlayId + 'apply';
+    document.getElementById(openModal).style.display = 'flex';
+    document.getElementById(openOverlay).style.display = 'flex';
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,19 @@ module.exports = {
     "./views/**/*.{html,js}"
   ],
   theme: {
+    colors: {
+      'german-yellow': '#FFCF00',
+      'german-red': '#DE0000',
+      'german-black': '#000000',
+      'german-white': '#FFFFFF',
+      'black-red': '#0A0202',
+      'white-yellow': '#FFFDF2',
+      'dark-red-2': '#890E07',
+      'dark-red-3': '#700A06',
+      'dark-red-4': '#550702',
+      'dark-red-5': '#9F090A',
+      'dark-red-6': '#FD0100',
+    },
     extend: {},
   },
   plugins: [],
