8a9e333eacf459c886f5273da5dd51bdcab5a8b6
Marck-road <109259291+Marck-road@users.noreply.github.com>
2024-03-25 13:25:58 +0800

    Tailwind Refactor, Register Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,16 @@ module.exports = {
     "./views/**/*.{html,js}"
   ],
   theme: {
+    fontSize:{
+      'modal-head': ['1.5rem', '2rem'],
+      sm: '0.8rem',
+      base: '1rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+    },
     colors: {
       'german-yellow': '#FFCF00',
       'german-red': '#DE0000',
